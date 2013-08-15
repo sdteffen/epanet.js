cbcee9276fa1a824ab5f33b6157dbf3adc686d5b
Steffen Macke <sdteffen@sdteffen.de>
2013-08-15 09:19:03 +0200

    Further layout progress.

--- src/pre.js
+++ src/pre.js
@@ -5,6 +5,7 @@ var preRun = function() {
     try
     {
         var inp = document.getElementById('inputTextarea').value;
+        $('#inputModal').modal('hide');
         $('#inputPre').html(inp);
         FS.createDataFile('/', 'input.inp', inp, true, true);
     } catch (e) {
@@ -13,11 +14,16 @@ var preRun = function() {
     rendersvg();
 },
         postRun = function() {
-    var t = Module.intArrayToString(FS.findObject('/report.txt').contents)
+    var t = Module.intArrayToString(FS.findObject('/report.txt').contents),
+        s = $('#status'),
+        m = s.html(),
+        l = (0 > m.indexOf('error') ? '<span class="label label-success">Success</span>' 
+            : '<span class="label label-important">Error</span>');
+    s.html(l + m.replace(/^[ \n]*\.\.\. */, ' '));
     t = t.replace(/&/g, "&amp;");
     t = t.replace(/</g, "&lt;");
     t = t.replace(/>/g, "&gt;");
-    $('#output').html(t);
+    $('#output').html(t);    
 },
         Module = {
     arguments: ['/input.inp', '/report.txt', '/report.bin'],
