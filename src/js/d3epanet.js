56bba181d67e1dd1a6643cc3dfeb20c1df89482b
Steffen Macke <sdteffen@sdteffen.de>
2013-08-18 16:51:57 +0200

    Rendering polylines, labels.

--- src/js/d3epanet.js
+++ src/js/d3epanet.js
@@ -15,16 +15,10 @@ d3.inp = function() {
                 if (m && m.length && 3 == m.length)
                     section[key] = {x: parseFloat(m[1]), y: parseFloat(m[2])};
             },
-            VERTICES: function(section, key, line) {
-                var m = line.match(/\s*([0-9\.]+)\s+([0-9\.]+)/)
-                v = section[key] || [];
-                c = {};
-                if (m && m.length && 3 == m.length) {
-                    c.x = parseFloat(m[1]);
-                    c.y = parseFloat(m[2]);
-                }
-                v[v.length] = c;
-                section[key] = v;
+            LABELS: function(section, key, line) {
+                var m = line.match(/\s+([[0-9\.]+)\s+"([^"]+)"/);
+                if (m && m.length && 3 == m.length)
+                    section[section.length] = {x: parseFloat(key), y: parseFloat(m[1]), label: m[2]};
             },
             PIPES: function(section, key, line) {
                 var m = line.match(/\s*([^\s;]+)\s+([^\s;]+)\s+([0-9\.]+)\s+([0-9\.]+)\s+([0-9\.]+)\s+([0-9\.]+)\s+([^;]).*/);
@@ -33,7 +27,6 @@ d3.inp = function() {
                         DIAMETER: parseFloat(m[4]), ROUGHNESS: parseFloat(m[5]),
                         MINORLOSS: parseFloat(m[6]), STATUS: m[7]};
                 }
-
             },
             PUMPS: function(section, key, line) {
                 var m = line.match(/\s*([^\s;]+)\s+([^\s;]+)\s+([^;]+).*/);
@@ -48,9 +41,20 @@ d3.inp = function() {
                         TYPE: m[4], SETTING: m[5],
                         MINORLOSS: parseFloat(m[6])};
                 }   
+            },            
+            VERTICES: function(section, key, line) {
+                var m = line.match(/\s*([0-9\.]+)\s+([0-9\.]+)/)
+                v = section[key] || [];
+                c = {};
+                if (m && m.length && 3 == m.length) {
+                    c.x = parseFloat(m[1]);
+                    c.y = parseFloat(m[2]);
+                }
+                v[v.length] = c;
+                section[key] = v;
             }
         },
-        model = {COORDINATES: {}, RESERVOIRS: {}, TANKS: {}},
+        model = {COORDINATES: {}, LABELS: [], RESERVOIRS: {}, TANKS: {}},
         lines = text.split(/\r\n|\r|\n/),
                 section = null;
         lines.forEach(function(line) {
@@ -58,7 +62,8 @@ d3.inp = function() {
                 return;
             } else if (regex.section.test(line)) {
                 var s = line.match(regex.section);
-                model[s[1]] = {};
+                if('undefined' == typeof model[s[1]])
+                    model[s[1]] = {};
                 section = s[1];
             } else if (regex.value.test(line)) {
                 var v = line.match(regex.value);
@@ -88,6 +93,7 @@ function svgRemoveAll(svg) {
     svg.selectAll('circle').remove();
     svg.selectAll('rect').remove();
     svg.selectAll('polygon').remove();
+    svg.selectAll('text').remove();
 }
 
 function svgTooltip(element) {
@@ -100,7 +106,7 @@ function svgTooltip(element) {
         .attr('y', parseFloat(a['cy'].value)-r)
         .text(a['title'].value)
         .attr('class', 'svgtooltip')
-        .attr('style', 'font-family: Verdana, Arial, sans; font-size:'+(r*4))
+        .attr('style', 'font-family: Verdana, Arial, sans; font-size:'+(r*2))
         .attr('fill', 'white');
 }
 
@@ -125,17 +131,33 @@ function rendersvg() {
         return c.y
     },
             minx = d3.min(coords, x),
+            maxx = d3.max(coords, x),
             miny = d3.min(coords, y),
             maxy = d3.max(coords, y),
             height = (maxy - miny),
-            width = (d3.max(coords, x) - minx),
+            width = (maxx - minx),
             scale = width * 0.2,
             top = maxy + scale,
             nodeSize = height / 75,
             strokeWidth = height / 200;
 
     svg.attr('viewBox', (minx - scale) + ' ' + (-0.7 * scale) + ' ' + (width + 0.5 * scale) + ' ' + (height + 2 * scale));
+    
+    svg.append('circle')
+        .attr('cx', minx + width/2)
+        .attr('cy', top - height/2)
+        .attr('r', nodeSize)
+        .attr('style', 'fill: black');
+    var c = d3.select('circle');
+    if(c && c[0] && c[0][0] && c[0][0].getBoundingClientRect)
+    {        
+        var r = c[0][0].getBoundingClientRect();
+        if(r && r.height && r.width)
+            nodeSize = nodeSize / r.height * 8;        
+    }
+    svgRemoveAll(svg);
 
+    // Render nodes
     for (var coordinate in model.COORDINATES)
     {
         var c = model.COORDINATES[coordinate];
@@ -163,7 +185,8 @@ function rendersvg() {
                 .attr('style', 'fill: white;');
         }
     }
-
+    
+    // Render links
     for (var section in linksections) {
         var s = linksections[section];       
         if (model[s]) {
@@ -177,14 +200,33 @@ function rendersvg() {
                     var centerx = (c1.x+c2.x)/2,
                         centery = (c1.y+c2.y)/2,
                         angle = 180/Math.PI*Math.atan2(c1.y-c2.y, c2.x-c1.x),
-                            transform = 'rotate('+angle+' '+centerx+' '+(top-centery)+')'; ;
-                    svg.append('line')
+                            transform = 'rotate('+angle+' '+centerx+' '+(top-centery)+')';
+                    if(model['VERTICES'][link]) {
+                        // Render polylines                        
+                        var v = model['VERTICES'][link],
+                            d = 'M '+c1.x+' '+(top-c1.y);
+                        for(var point in v) {
+                            var p = v[point];
+                            d = d + ' L '+p.x+' '+(top-p.y);
+                        }
+                        d = d + ' L '+c2.x+' '+(top-c2.y);
+                        svg.append('path')
+                            .attr('stroke', 'white')
+                            .attr('fill', 'none')
+                            .attr('d', d)
+                            .attr('stroke-width', strokeWidth);                        
+
+                    }
+                    else
+                    {
+                        svg.append('line')
                             .attr('x1', c1.x)
                             .attr('y1', top-c1.y)
                             .attr('x2', c2.x)
                             .attr('y2', top-c2.y)
                             .attr('stroke', 'white')
                             .attr('stroke-width', strokeWidth);
+                    }
                     
                     if('PUMPS' == s) {
                         svg.append('circle')
@@ -217,7 +259,18 @@ function rendersvg() {
             }
         }
     }
-
+    // Render labels
+    for(var label in model['LABELS']) {
+        var l = model['LABELS'][label],
+            t = l['label'];
+        svg.append('text')
+        .attr('x', l['x']-nodeSize*t.length/3)
+        .attr('y', top-l['y']+nodeSize*2)
+        .text(t)
+        .attr('style', 'font-family: Verdana, Arial, sans; font-size:'+(nodeSize*2))
+        .attr('fill', 'white');
+    }
+      
 }
 
 function loadSample(f) {
